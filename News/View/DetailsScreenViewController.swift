ca1d2d9967a26fac8c9fc796a7c7c955a22bc463
Ahmed <ahmed.sharf.777@gmail.com>
2021-08-26 22:00:07 +0100

    display data in Details screen enhancement UI

--- News/View/DetailsScreenViewController.swift
+++ News/View/DetailsScreenViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import SDWebImage
 
 class DetailsScreenViewController: UIViewController {
-    
     
     @IBOutlet weak var articleImageView: UIImageView!
     @IBOutlet weak var outterContainerView: UIView!
@@ -22,9 +22,19 @@ class DetailsScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        articleImageView.sd_setImage(with: URL(string: article.urlToImage ?? ""), placeholderImage: UIImage(named: "placeholder"))
+        articleSourceNameLabel.text = article.source.name
+        articleTitleLabel.text = article.title
+        articleDescriptionLabel.text = article.description
         
-        print("Details: \(article.title)")
+        outterContainerView.layer.cornerRadius = 15
+        innerContainerView.layer.cornerRadius = 15
         
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)
+        navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationController?.navigationBar.topItem?.backBarButtonItem?.tintColor = #colorLiteral(red: 0.03529411765, green: 0.368627451, blue: 0.6705882353, alpha: 1)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: nil)
+        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.03529411765, green: 0.368627451, blue: 0.6705882353, alpha: 1)
     }
-    
 }
